716e132225fba70e20d10c6c99a34e06a3f8ebb2
Napolux <napolux@gmail.com>
2024-06-20 17:24:28 +0200

    cloudflare setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
 });
