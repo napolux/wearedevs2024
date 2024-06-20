5ded23ea355f97d2ba859f61bc365487c57038ea
Napolux <napolux@gmail.com>
2024-06-20 12:18:45 +0200

    adding templates, page and layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable React to support React JSX components.
   integrations: [react(), tailwind()]
 });
